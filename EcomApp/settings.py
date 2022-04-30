451417fcbbfbd3de5fb7075b72e337af0e512b9c
Md Imran Hossain <mdimranh750@gmail.com>
2022-04-30 21:41:03 +0600

    dfdsf

--- EcomApp/settings.py
+++ EcomApp/settings.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import django_heroku
 import dj_database_url
 
+os.environ['DATABASE_URL']
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
