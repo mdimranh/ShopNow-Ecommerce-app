98a60bcddcf21044d43be7f29bce31fd92162620
Md Imran Hossain <mdimranh750@gmail.com>
2022-05-01 17:46:08 +0600

    djghksdaf

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
-from .models import Profile, AddressBook, EmailConfirmed
+from .models import UserProfile, AddressBook, EmailConfirmed
 
 # admin.site.unregister(Group)
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['name', 'phone',]
-admin.site.register(Profile, ProfileAdmin)
+admin.site.register(UserProfile, ProfileAdmin)
 admin.site.register(AddressBook)
 admin.site.register(EmailConfirmed)
