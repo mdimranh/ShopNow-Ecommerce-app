ce90098cebf87b487b127db93a4a979238d401c2
Md Imran Hossain <mdimranh750@gmail.com>
2022-05-01 10:20:02 +0600

    bug fixed

--- newsletter/views.py
+++ newsletter/views.py
@@ -7,8 +7,8 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.contrib.sites.shortcuts import get_current_site
 
-from setting.models import EmailConfig
-from control.emailconfig import backend
+# from setting.models import EmailConfig
+# from control.emailconfig import backend
 
 class AddNewsletterEmail(View):
     def post(self, request, *args, **kwargs):
@@ -20,18 +20,17 @@ class AddNewsletterEmail(View):
             nlemail.request = request
             nlemail.save()
             type = 'add'
-            try:
-                return JsonResponse({'type': type})
-            finally:
-                email_config = EmailConfig.objects.get()
-                subject, from_email, to = 'Welcome to newsletter', email_config.email_host_user, email
-                text_content = 'Welcome to newsletter.'
-                html_content = render_to_string('newsletter/welcome-newsletter.html', context={
-                    'domain': get_current_site(request).domain
-                })
-                msg = EmailMultiAlternatives(subject, text_content, from_email, [to], connection=backend)
-                msg.attach_alternative(html_content, "text/html")
-                msg.send()
+            return JsonResponse({'type': type})
+            # finally:
+            #     email_config = EmailConfig.objects.get()
+            #     subject, from_email, to = 'Welcome to newsletter', email_config.email_host_user, email
+            #     text_content = 'Welcome to newsletter.'
+            #     html_content = render_to_string('newsletter/welcome-newsletter.html', context={
+            #         'domain': get_current_site(request).domain
+            #     })
+            #     msg = EmailMultiAlternatives(subject, text_content, from_email, [to], connection=backend)
+            #     msg.attach_alternative(html_content, "text/html")
+            #     msg.send()
         else:
             type = 'exist'
             return JsonResponse({'type': type})
