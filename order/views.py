ef27a25074424fb08c9e8637bac2865f17c45997
Md Imran Hossain <mdimranh750@gmail.com>
2022-05-08 06:54:54 +0600

    dsfdgsdf

--- order/views.py
+++ order/views.py
@@ -364,7 +364,7 @@ def WishItemDelete(request):
 
 def PlaceOrder(request):
 	if request.method == "POST":
-		if request.POST['address_book'] == 'None':
+		if request.POST['diff-address'] == 'off':
 			country = Country.objects.get(id = request.POST['add-country']).name
 			region = Region.objects.get(id = request.POST['add-region']).name
 			city = City.objects.get(id = request.POST['add-city']).name
