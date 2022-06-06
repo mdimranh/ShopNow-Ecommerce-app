4698d42cd65b53d1ec39d4e675ab60acf3cbf666
Md Imran Hossain <mdimranh750@gmail.com>
2022-06-06 09:07:48 +0600

    api.views.py file updated

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
-from .models import UserProfile, AddressBook, EmailConfirmed
+from .models import Profile, AddressBook, EmailConfirmed
 
 # admin.site.unregister(Group)
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['name', 'phone',]
-admin.site.register(UserProfile, ProfileAdmin)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(AddressBook)
 admin.site.register(EmailConfirmed)
